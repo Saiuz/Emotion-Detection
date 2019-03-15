638f7e71139229c84b7184bb1d6d3fa4bb04d420
ablacklama <ablacklama@gmail.com>
2019-03-15 00:57:07 -0700

    fixing stuff, added todo list

--- Expression_Network.py
+++ Expression_Network.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 class Expression_Network(object):
     def __init__(self):
         #hardcode in num_classes
-        self.num_classes = 8
+        self.num_classes = 4
         tf.reset_default_graph()
         self.x = tf.placeholder(dtype=tf.float32, shape=(None,128,128),name='x')
         self.model(self.x)
@@ -31,52 +31,51 @@ class Expression_Network(object):
         self.conv = tf.contrib.layers.conv2d(
             self.reshaped,
             32,
-            (9, 9),
-            stride=(1, 1),
+            (9,9),
+            stride=(1,1),
             padding='VALID',
             weights_initializer=tf.initializers.random_normal)
-        # batch,120,120,32
+        #batch,120,120,32
 
-        # subsample
+        #subsample
         self.pool = tf.contrib.layers.max_pool2d(
             self.conv,
-            (4, 4),
-            stride=(4, 4))
-        # batch,30,30,32
+            (4,4),
+            stride=(4,4))
+        #batch,30,30,32
 
-        # second convolution
+        #second convolution
         self.conv2 = tf.contrib.layers.conv2d(
             self.pool,
             64,
-            (11, 11),
-            stride=(1, 1),
+            (11,11),
+            stride=(1,1),
             padding='VALID',
             weights_initializer=tf.initializers.random_normal)
-        # batch,20,20,64
+        #batch,20,20,64
 
-        # subsample
+        #subsample
         self.pool2 = tf.contrib.layers.max_pool2d(
             self.conv2,
-            (4, 4),
-            stride=(4, 4))
-        # batch,5,5,64
+            (4,4),
+            stride=(4,4))
+        #batch,5,5,64
 
-        # flatten
+        #flatten
         self.flat = tf.contrib.layers.flatten(self.pool2)
-        # batch,1600
+        #batch,1600
 
-        # fully connected
+        #fully connected
         self.fc = tf.contrib.layers.fully_connected(
             self.flat,
-            1024,
-            activation_fn=tf.nn.relu,
-            weights_initializer=tf.initializers.random_normal)
-        # batch,256
+            256,
+            activation_fn=tf.nn.leaky_relu)
+
+        #batch,256
         self.logits = tf.contrib.layers.fully_connected(
             self.fc,
             self.num_classes,
-            weights_initializer=tf.initializers.random_normal,
             activation_fn=None)
 
-        self.prediction = tf.nn.softmax(self.logits, )
-        
+        self.prediction = tf.nn.softmax(self.logits,)
+
