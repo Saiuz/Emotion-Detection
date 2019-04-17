5421be3045eb0d0f483789277fd9c95ade2ae7f1
ablacklama <ablacklama@gmail.com>
2019-04-16 21:12:12 -0700

    updated notebooks. removed old code, commented, the whole shabang

--- Expression_Network.py
+++ Expression_Network.py
@@ -1,6 +1,6 @@
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Reshape, Dropout
+from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D, BatchNormalization, Reshape, Dropout
 import traceback
 
 class Expression_Network(object):
@@ -33,15 +33,17 @@ class Expression_Network(object):
         return self.model.predict([img])
 
     def build_model(self):
-        # batch,128,128,1
         model = tf.keras.models.Sequential([
+            # greyscale images don't have a third channel, so in order for conv2D to accept them, we need to
+            # add another dimension. This doesn't change the contents of the picture, just the format.
             Reshape((128,128,1),input_shape=(128,128)),      
             Conv2D(32, 9, activation='relu'),
             MaxPooling2D(pool_size=4),
             BatchNormalization(),
             Conv2D(64, 11, activation='relu'),
             MaxPooling2D(pool_size=4),
-            Dropout(.2),
+            Dropout(rate=.2),
+            # Flatten our data down to 1 dimension so it can be fed into our dense layers
             Flatten(),
             Dense(256, activation=tf.nn.leaky_relu, kernel_regularizer=tf.keras.regularizers.l2(0.001)),
             Dense(self.num_classes,activation="softmax", kernel_regularizer=tf.keras.regularizers.l2(0.001))
